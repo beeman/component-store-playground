c9c8b6e7d51ad54b05d92423d375dc54b6213d8d
Bram Borggreve <borggreve@gmail.com>
2020-12-05 22:59:55 -0500

    chore: remove @angular/fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyDsXRmuQ1vL0Mizgz7w1SlKs0EZi10IGZg',
-    authDomain: 'component-store-playground.firebaseapp.com',
-    databaseURL: 'https://component-store-playground.firebaseio.com',
-    projectId: 'component-store-playground',
-    storageBucket: 'component-store-playground.appspot.com',
-    messagingSenderId: '285227042544',
-    appId: '1:285227042544:web:153da345c2ec91acca6435',
-    measurementId: 'G-064J726JQF',
-  },
 }
 
 /*
