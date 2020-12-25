1452e78fb33e4f551d304f36f598c10b380523e6
Chau Tran <nartc7789@gmail.com>
2020-12-25 08:48:08 -0600

    chore: remove comments

--- libs/playground/shell/src/lib/playground-shell.routes.ts
+++ libs/playground/shell/src/lib/playground-shell.routes.ts
@@ -39,33 +39,3 @@ export const playgroundShellRoutes: Routes = [
     ],
   },
 ]
-
-/**
- {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      { path: '', pathMatch: 'full', redirectTo: 'home' },
-      {
-        path: 'auth',
-        loadChildren: () => import('./features/auth/auth.module').then((m) => m.AuthModule),
-      },
-      {
-        path: 'forms',
-        loadChildren: () => import('./features/forms-demo/forms-demo.module').then((m) => m.FormsDemoModule),
-      },
-      {
-        path: 'home',
-        loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule),
-      },
-      {
-        path: 'todos',
-        loadChildren: () => import('./features/todos/todos.module').then((m) => m.TodosModule),
-      },
-      {
-        path: 'workflows',
-        loadChildren: () => import('./features/workflows/workflows.module').then((m) => m.WorkflowsModule),
-      },
-    ],
-  }
- */
