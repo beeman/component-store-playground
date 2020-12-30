11be5fb0bae54d95014610bfa99e37d003793230
Chau Tran <nartc7789@gmail.com>
2020-12-29 21:30:30 -0600

    fix: adjust workflow

--- libs/shared/util/custom-component-store/src/lib/shared-util-custom-component-store.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sharedUtilCustomComponentStore } from './shared-util-custom-component-store'
-
-describe('sharedUtilCustomComponentStore', () => {
-  it('should work', () => {
-    expect(sharedUtilCustomComponentStore()).toEqual('shared-util-custom-component-store')
-  })
-})
